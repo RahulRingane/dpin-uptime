d901e1911e45e571bc76e8ff5a34e77631e341f6
Rahul Ringane <rahul897r@gmail.com>
2025-03-16 19:25:25 +0530

    Initial setup with schema

---
+++ apps/api/index.ts
@@ -0,0 +1,22 @@
+import express from "express";
+
+const app = express();
+
+app.post("/api/v1/website", (req, res) => {
+
+});
+
+app.post("/api/v1/website/status", (req, res) => {
+
+});
+
+app.get("/api/v1/websites", (req, res) => {
+
+});
+
+app.delete("/api/v1/website/", (req, res) => {
+
+});
+
+
+app.listen(3000);
